82b275481fa71357371895d647a2d048c6c04cab
= <zahrashabani2000125@gmial.com>
2023-02-15 12:40:47 +0330

    Added profiles viewset

--- profiles_project/profiles_api/urls.py
+++ profiles_project/profiles_api/urls.py
@@ -5,7 +5,7 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
-
+router.register('profile', views.UserProfileViewSet)
 urlpatterns = [
     path('hellow-view', views.HelloApiView.as_view()),
     path('', include(router.urls)),
